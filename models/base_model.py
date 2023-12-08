3d296e672a35e459012b14620109d34a5f4e0563
ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-08 23:11:25 +0100

    test codes

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,9 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    else:
-                        setattr(self, key, value)    
+                    self.__dict__[key] = value
+                elif key in ['created_at', 'updated_at']:
+                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")   
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
--- tests/test_models/test_base_modale.py
+++ tests/test_models/test_base_modale.py
@@ -2,15 +2,8 @@ import unittest
 from unittest.mock import patch
 from datetime import datetime
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
 
 class TestBaseModel(unittest.TestCase):
-
-    def setUp(self):
-        # Patch FileStorage to avoid writing to the file during tests
-        patcher = patch('models.engine.file_storage.FileStorage._FileStorage__file_path', new_callable=str)
-        self.addCleanup(patcher.stop)
-        self.mocked_file_path = patcher.start()
 
     def test_base_model_init(self):
         """Test initialization of BaseModel."""
@@ -29,15 +22,12 @@ class TestBaseModel(unittest.TestCase):
 
     def test_base_model_save(self):
         """Test save method of BaseModel."""
-        with patch('models.engine.file_storage.FileStorage.save') as mock_save:
-            new_model = BaseModel()
-            old_updated_at = new_model.updated_at
-            new_model.save()
+        new_model = BaseModel()
+        old_updated_at = new_model.updated_at
+        new_model.save()
 
-            # Check if updated_at is updated after save
-            self.assertNotEqual(old_updated_at, new_model.updated_at)
-            # Check if FileStorage save method is called
-            mock_save.assert_called_once()
+        # Check if updated_at is updated after save
+        self.assertNotEqual(old_updated_at, new_model.updated_at)
 
     def test_base_model_to_dict(self):
         """Test to_dict method of BaseModel."""
@@ -65,17 +55,13 @@ class TestBaseModel(unittest.TestCase):
             'name': 'Test Model'
         }
 
-        with patch('models.engine.file_storage.FileStorage.new') as mock_new:
-            new_model = BaseModel(**data)
+        new_model = BaseModel(**data)
 
-            # Check if attributes are set correctly
-            self.assertEqual(new_model.id, 'test_id')
-            self.assertEqual(new_model.created_at, datetime(2023, 1, 1, 12, 0, 0))
-            self.assertEqual(new_model.updated_at, datetime(2023, 1, 1, 12, 30, 0))
-            self.assertEqual(new_model.name, 'Test Model')
-
-            # Check if FileStorage new method is called
-            mock_new.assert_called_once_with(new_model)
+        # Check if attributes are set correctly
+        self.assertEqual(new_model.id, 'test_id')
+        self.assertEqual(new_model.created_at, datetime(2023, 1, 1, 12, 0, 0))
+        self.assertEqual(new_model.updated_at, datetime(2023, 1, 1, 12, 30, 0))
+        self.assertEqual(new_model.name, 'Test Model')
 
 if __name__ == '__main__':
     unittest.main()

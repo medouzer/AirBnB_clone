4a8ec902601cc16ef3658f930f5425b025995987
ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-08 23:56:00 +0100

    to_dict modif

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,10 @@ class BaseModel:
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        
+        if isinstance(obj_dict['created_at'], datetime):
+            obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        if isinstance(obj_dict['updated_at'], datetime):
+            obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
         
         return obj_dict

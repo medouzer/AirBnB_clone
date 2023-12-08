404277b547d5b86ee842f50ffba373a91fb3cb9a
ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-08 22:40:26 +0100

    test of the new things

--- tests/test_models/test_base_modale.py
+++ tests/test_models/test_base_modale.py
@@ -2,8 +2,15 @@ import unittest
 from unittest.mock import patch
 from datetime import datetime
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        # Patch FileStorage to avoid writing to the file during tests
+        patcher = patch('models.engine.file_storage.FileStorage._FileStorage__file_path', new_callable=str)
+        self.addCleanup(patcher.stop)
+        self.mocked_file_path = patcher.start()
 
     def test_base_model_init(self):
         """Test initialization of BaseModel."""
@@ -22,12 +29,15 @@ class TestBaseModel(unittest.TestCase):
 
     def test_base_model_save(self):
         """Test save method of BaseModel."""
-        new_model = BaseModel()
-        old_updated_at = new_model.updated_at
-        new_model.save()
+        with patch('models.engine.file_storage.FileStorage.save') as mock_save:
+            new_model = BaseModel()
+            old_updated_at = new_model.updated_at
+            new_model.save()
 
-        # Check if updated_at is updated after save
-        self.assertNotEqual(old_updated_at, new_model.updated_at)
+            # Check if updated_at is updated after save
+            self.assertNotEqual(old_updated_at, new_model.updated_at)
+            # Check if FileStorage save method is called
+            mock_save.assert_called_once()
 
     def test_base_model_to_dict(self):
         """Test to_dict method of BaseModel."""
@@ -63,6 +73,12 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(new_model.updated_at, datetime(2023, 1, 1, 12, 30, 0))
         self.assertEqual(new_model.name, 'Test Model')
 
+        # Additional check for integration with FileStorage
+        with patch('models.engine.file_storage.FileStorage.new') as mock_new:
+            new_model = BaseModel(**data)
+
+            # Check if FileStorage new method is called
+            mock_new.assert_called_once_with(new_model)
+
 if __name__ == '__main__':
     unittest.main()
-

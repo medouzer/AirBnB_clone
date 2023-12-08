20038b844715ede58d938a9cccbbe11dbf61bb88
ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-08 23:46:03 +0100

    reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,14 +22,32 @@ class FileStorage:
         with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file)
 
+    # def reload(self):
+    #     try:
+    #         with open(self.__file_path, 'r') as file:
+    #             data = json.load(file)
+    #             for key, obj_data in data.items():
+    #                 class_name = obj_data['__class__']
+    #                 del obj_data['__class__']
+    #                 obj_instance = eval(class_name)(**obj_data)
+    #                 self.__objects[key] = obj_instance
+    #     except FileNotFoundError:
+    #         pass
     def reload(self):
         try:
             with open(self.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, obj_data in data.items():
-                    class_name = obj_data['__class__']
-                    del obj_data['__class__']
-                    obj_instance = eval(class_name)(**obj_data)
-                    self.__objects[key] = obj_instance
+                file_content = file.read()
+                print("File Content:", file_content)  # Add this line for debugging
+                if file_content.strip():  # Check if the file is not empty
+                    data = json.loads(file_content)
+                    for key, obj_data in data.items():
+                        class_name = obj_data['__class__']
+                        del obj_data['__class__']
+                        obj_instance = eval(class_name)(**obj_data)
+                        self.__objects[key] = obj_instance
         except FileNotFoundError:
+            # File not found, no need for an error
             pass
+        except json.decoder.JSONDecodeError as e:
+            # Handle JSON decoding error (if any)
+            print(f"Error decoding JSON: {e}")

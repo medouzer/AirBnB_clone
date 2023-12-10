b7a1e115eb7355f7a257f4ff243f11d2763ed697
EL HADDOUMI <125677762+elhaddoumi1999@users.noreply.github.com>
2023-12-10 13:28:32 +0100

    Delete models directory

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Amenty class"""
-
-from models.base_model import BaseModel
-
-class Amenity(BaseModel):
-    """Amenity class that inherits from BaseModel"""
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""State class"""
-
-from models.base_model import BaseModel
-
-class State(BaseModel):
-    """state class that inherits from BaseModel"""
-    name = ""

12f43bc4c00a8b21d0f4f470e8d48246ef6b2b6d
ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-11 21:10:15 +0100

    check the reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,32 +30,56 @@ class FileStorage:
         with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file)
 
-    def reload(self):
-        if os.path.exists(self.__file_path):
-            try:
-                with open(self.__file_path, 'r') as file:
-                    file_content = file.read()
-                    if file_content.strip():  # Check if the file is not empty
-                        data = json.loads(file_content)
-                        for key, obj_data in data.items():
-                            class_name = obj_data['__class__']
-                            del obj_data['__class__']
+    # def reload(self):
+    #     if os.path.exists(self.__file_path):
+    #         try:
+    #             with open(self.__file_path, 'r') as file:
+    #                 file_content = file.read()
+    #                 if file_content.strip():  # Check if the file is not empty
+    #                     data = json.loads(file_content)
+    #                     for key, obj_data in data.items():
+    #                         class_name = obj_data['__class__']
+    #                         del obj_data['__class__']
 
-                            class_mapping = {
-                                'BaseModel': BaseModel,
-                                'User': User,
-                                'State': State,
-                                'City': City,
-                                'Amenity': Amenity,
-                                'Place': Place,
-                                'Review': Review
-                            }
+    #                         class_mapping = {
+    #                             'BaseModel': BaseModel,
+    #                             'User': User,
+    #                             'State': State,
+    #                             'City': City,
+    #                             'Amenity': Amenity,
+    #                             'Place': Place,
+    #                             'Review': Review
+    #                         }
 
-                            obj_instance = class_mapping[class_name](
-                                **obj_data)
-                            self.__objects[key] = obj_instance
-            except Exception:
-                pass
+    #                         obj_instance = class_mapping[class_name](
+    #                             **obj_data)
+    #                         self.__objects[key] = obj_instance
+    #         except Exception:
+    #             pass
+        def reload(self):
+            """ doc doc """
+            filepath = FileStorage.__file_path
+            data = FileStorage.__objects
+            if os.path.exists(filepath):
+                try:
+                    with open(filepath) as f:
+                        for key, value in json.load(f).items():
+                            if "BaseModel" in key:
+                                data[key] = BaseModel(**value)
+                            if "User" in key:
+                                data[key] = User(**value)
+                            if "Place" in key:
+                                data[key] = Place(**value)
+                            if "State" in key:
+                                data[key] = State(**value)
+                            if "City" in key:
+                                data[key] = City(**value)
+                            if "Amenity" in key:
+                                data[key] = Amenity(**value)
+                            if "Review" in key:
+                                data[key] = Review(**value)
+                except Exception:
+                    pass
 
     # def reload(self):
     #     if os.path.exists(self.__file_path):
